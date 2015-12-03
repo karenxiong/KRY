1a4ed0a2aa5a3e3308e12bbb56e890a3e47b95a4
karenxiong <karen.xiong@mail.utoronto.ca>
2015-12-03 03:03:22 -0500

    Added imgs, bgs, etc.
    placement backgrounds + vector social media icons, etc.

--- S1P2/js/main.js
+++ S1P2/js/main.js
@@ -2,12 +2,11 @@ $(document).ready(function() {
 	var $window = $(window);
 
 	$('section[data-type="background"]').each(function(){
-		console.log("does something");
 		// background object
 		var $bgobj = $(this);
 		$(window).scroll(function() {
 			var yPos = -($window.scrollTop() / $bgobj.data("speed"));
-			var coords = '50%' + yPos +'px'
+			var coords = '80%' + yPos +'px'
 			$bgobj.css({backgroundPosition: coords});
 		});
 	});
