8595f129204ee922a69bb086a19d173e0a7979dd
karenxiong <karen.xiong@mail.utoronto.ca>
2015-12-06 23:12:29 -0500

    Added navi, links, etc

--- S1P2/js/main.js
+++ S1P2/js/main.js
@@ -1,13 +1,12 @@
-$(document).ready(function() {
-	var $window = $(window);
+// $(document).ready(function() {
+// 	var $window = $(window);
 
-	$('section[data-type="background"]').each(function(){
-		// background object
-		var $bgobj = $(this);
-		$(window).scroll(function() {
-			var yPos = -($window.scrollTop() / $bgobj.data("speed"));
-			var coords = '80%' + yPos +'px'
-			$bgobj.css({backgroundPosition: coords});
-		});
-	});
-});
+// 	$('section[data-type="background"]').each(function(){
+// 		// background object
+// 		var $bgobj = $(this);
+// 		$(window).scroll(function() {
+// 			var yPos = -($window.scrollTop() / $bgobj.data("speed"));
+// 			var coords = '80%' + yPos +'px'
+// 			$bgobj.css({backgroundPosition: coords});
+// 		});
+// 	});
