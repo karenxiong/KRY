6f079cebeab33b3370180594dc061126e93cc458
karenxiong <karen.xiong@mail.utoronto.ca>
2015-12-07 12:46:37 -0500

    Fixed some issues

--- S1P2/js/main.js
+++ S1P2/js/main.js
@@ -1,12 +1,21 @@
-// $(document).ready(function() {
-// 	var $window = $(window);
+$(document).ready(function() {
+	// var $window = $(window);
 
-// 	$('section[data-type="background"]').each(function(){
-// 		// background object
-// 		var $bgobj = $(this);
-// 		$(window).scroll(function() {
-// 			var yPos = -($window.scrollTop() / $bgobj.data("speed"));
-// 			var coords = '80%' + yPos +'px'
-// 			$bgobj.css({backgroundPosition: coords});
-// 		});
-// 	});
+	// $('section[data-type="background"]').each(function(){
+	// 	// background object
+	// 	var $bgobj = $(this);
+	// 	$(window).scroll(function() {
+	// 		var yPos = -($window.scrollTop() / $bgobj.data("speed"));
+	// 		var coords = '80%' + yPos +'px'
+	// 		$bgobj.css({backgroundPosition: coords});
+	// 	});
+	// });
+	$(function() {
+		$('a').each(function() {
+			if($(this).prop('href') == window.location.href) {
+			$(this).addClass('current');
+		}
+		});
+	});
+
+});
