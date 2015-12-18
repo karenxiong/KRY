8e7c34430a00bfaa9514684db324695981a0501b
karenxiong <karen.xiong@mail.utoronto.ca>
2015-12-18 01:49:36 -0500

    finished!

--- S1P2/js/main.js
+++ S1P2/js/main.js
@@ -18,12 +18,22 @@ $(document).ready(function() {
       };
     },
     function() {
-      console.log($(this).css("background-image").indexOf("submitted.png"));
       if ($(this).css("background-image").indexOf("submitted.png") == -1) {
         $(this).css("background-image", "url(./img/preorder.png)");
       };
     });
 
+  /* Email Submit Alert & rollovers */
+  $("#emailSubmit").click(function() {
+    alert("Email Submitted");
+  });
+
+  $("#emailSubmit").hover(
+    function() {$(this).attr("src", "img/send2.png");}, 
+    function() {$(this).attr("src", "img/send.png");}
+  );
+
+  
   /* Scrolling Effects */
 
   //variable used to keep track of if the navbar has been clicked
@@ -41,27 +51,45 @@ $(document).ready(function() {
     //monitor when user clicks on the navbar
   $('#navigation a').click(function() {
     navClicked = true; // set tracker
-      //remove all highlights in navigation
-      $('#navigation a').removeClass('active');
 
-      //highlights the clicked navbar item
-      $(this).addClass('active');
+    //remove all highlights in navigation
+    $('#navigation a').removeClass('active');
 
-      //scrolls to the corresponding section
-      $('html, body').animate({ 
-        scrollTop: $("#" + $(this).html().replace(/ /g,'').toLowerCase()).offset().top + 'px' }, 1000, 'linear');
+    //highlights the clicked navbar item
+    $(this).addClass('active');
 
-      //enable the scroll function listener when all animations are done
-      $(":animated").promise().done(function() {
-        setTimeout(function() {navClicked = false;}, 500);
+    //scrolls to the corresponding section
+    $('html, body').animate({ 
+      scrollTop: $("#" + $(this).html().replace(/ /g,'').toLowerCase()).offset().top + 'px' }, 1000, 'linear');
+
+    //enable the scroll function listener when all animations are done
+    $(":animated").promise().done(function() {
+      setTimeout(function() {navClicked = false;}, 500);
     });
+  });
+
+  /* Scroll Up Arrow */
+
+  $('.arrowUp').click(function() {
+    navClicked = true; // set tracker
+
+    //remove all highlights in navigation
+    $('#navigation a').removeClass('active');
+
+    //scrolls to the corresponding section
+    $('html, body').animate({ 
+      scrollTop: $("#home").offset().top + 'px' }, 1000, 'linear');
+
+    //enable the scroll function listener when all animations are done
+    $(":animated").promise().done(function() {
+      setTimeout(function() {navClicked = false;}, 1000);
     });
+  });
 
   //keeps track of where the user is scrolling to and updates highlights on navbar
     $(document).scroll(function() {
       // only check scrolling if the navigation animations are done
       if (!navClicked) {
-        console.log("???")
         //remove all highlights in navigation
         $('#navigation a').removeClass('active');
 
@@ -71,7 +99,6 @@ $(document).ready(function() {
 
     // arrow scroll down effect
   $('.arrow').click(function(){
-    console.log("wow");
         var nextSection = $(this).closest('section').next('section');
         navClicked = true;
 
@@ -85,23 +112,6 @@ $(document).ready(function() {
         setTimeout(function() {navClicked = false;}, 500);
     });
     });
-
-  //arrow scroll up effect
-  $('.arrowUp').click(function(){
-    console.log("wow");
-        var nextSection = $(this).closest('section').next('section');
-        navClicked = true;
-
-        $('html, body').animate({
-            scrollTop: $(nextSection).offset().top
-        }, 1000);
-
-        //enable the scroll function listener when all animations are done
-      $(":animated").promise().done(function() {
-        setTimeout(function() {navClicked = false;}, 500);
-    });
-    });
-
 
   /* feature icons animation, for all icons */
   // mouse in to fade text in
@@ -130,13 +140,4 @@ $(document).ready(function() {
     $("#pre-order h2").html($(this).val());
   });
 
-  //social media button changes
-
-  $(".twitter").hover(
-    function() {
-      if ($(this).css("background-image").indexOf("twitter.png") == -1) {
-        $(this).css("background-image", "url(./img/twitter_hover.png)");
-      };
-    },
-  )
 });
