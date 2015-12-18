aa598f99353ff949d2cef59d4e1eb1b73babc667
karenxiong <karen.xiong@mail.utoronto.ca>
2015-12-17 22:50:25 -0500

    Lots changed
    added working canvas, added hover over items, fixed submit button, etc

--- S1P2/js/interactive.js
+++ S1P2/js/interactive.js
@@ -1,93 +1,130 @@
 $(document).ready(function() {
-  function interactive() {
-  $(".pButton").click(function() {
-    $("#popup").fadeIn();
-  });
-
-  }
-
-  $("a.closeButton").click(function() {
-    $("#popup").fadeOut();
-  });
-
-
-  $(".button").click(function() {
-    $(".button").fadeOut(1000, function() {
-      $(".canvas").fadeIn(0, function() {
+  
+  // ======== INTERACTIVE CANVAS ========
+  $(".button1").click(function() {
+    $(".button1").fadeOut(1000, function() {
+      $(".canvaschase").fadeIn(0, function() {
         // ========= MAN IMGS =========
         var man = new Image();
-        man.src = "img/man.png";
+        man.src = "img/man-stop.png";
+        var man1 = new Image();
+        man1.src = "img/man1.png";
+        var man2 = new Image();
+        man2.src = "img/man2.png";
+        var man3 = new Image();
+        man3.src = "img/man3.png";
+        var man4 = new Image();
+        man4.src = "img/man4.png";
+        var man5 = new Image();
+        man5.src = "img/man5.png";
 
         // ========= WOMAN IMGS =========
         var woman = new Image();
-        woman.src = "img/woman.png";
-
+        woman.src = "img/woman-stop.png";
         var woman1 = new Image();
         woman1.src = "img/woman1.png";
-
         var woman2 = new Image();
         woman2.src = "img/woman2.png";
+        var woman3 = new Image();
+        woman3.src = "img/woman3.png";
+        var woman4 = new Image();
+        woman4.src = "img/woman4.png";
+        var woman5 = new Image();
+        woman5.src = "img/woman5.png";
 
         // ========= CANVAS SET UP =========
         var canvas = document.getElementById("chase");
-        canvas.width = 700;
-        canvas.height = 450;
+        canvas.width = 600;
+        canvas.height = 600;
         var context = canvas.getContext("2d");
 
-        var x = 50;
-        var y = 50;
+        var x = 0;
+        var y = 150;
         var remain;
 
         var xMan = -200;
-        var yMan = 10;
+        var yMan = 150;
         var remainMan;
 
         var rightKeyIsDown = false;
         //var opacity = 0;
-        var opacity = 1;
+        var chaseOpacity = 1;
 
-        requestAnimationFrame(onAnimationFrame);
+
+        // ========= WALK IMGS =========
+        var manbehind = new Image();
+        manbehind.src = "img/manbehind.png";
+        
+        var womanfront = new Image();
+        womanfront.src = "img/womanfront.png";
+        
+        var heartpink = new Image();
+        heartpink.src = "img/heartpink.png";
+        
+        // ========= WALK CANVAS SET UP =========
+        var canvas = document.getElementById("walk");
+        canvas.width = 800;
+        canvas.height = 600;
+        var context = canvas.getContext("2d");
+
+        var walkOpacity = 0.5;
+        var xBehind = 0;
+        var v = 1;
+        var count = 0;
+    
+
+        requestAnimationFrame(chaseScenario);
 
         // ========= WOMAN WALKING MAN CHASING =========
 
-        function onAnimationFrame() {
+        function chaseScenario() {
           
           context.clearRect(0, 0, canvas.width, canvas.height);
 
           //the man is walking
           xMan = xMan + 1.4;
-          remainMan = xMan % 20;
-          if (remain < 10) {
-            context.drawImage(man, xMan, yMan, 140, 400);
-          } else if (remain >= 10) {
-            context.drawImage(man, xMan, yMan, 140, 400);
+          remainMan = (xMan + 200) % 50;
+          if (remainMan < 10) {
+            context.drawImage(man1, xMan, yMan);
+          } else if (remainMan < 20) {
+            context.drawImage(man2, xMan, yMan);
+          } else if (remainMan < 30) {
+            context.drawImage(man3, xMan, yMan);
+          } else if (remainMan < 40) {
+            context.drawImage(man4, xMan, yMan);
+          } else if (remainMan >= 40) {
+            context.drawImage(man5, xMan, yMan);
           } else {
-            context.drawImage(man, xMan, yMan, 140, 400);
+            context.drawImage(man, xMan, yMan);
           }
 
           //the woman stops or is walking
           if (rightKeyIsDown) {
             x = x + 1.5;
-            remain = x % 20;
-            console.log (x);
-            console.log (remain);
+            remain = x % 50;
             if (remain < 10) {
-              context.drawImage(woman1, x, y, 160, 360);
-            } else if (remain >= 10 ) {
-              context.drawImage(woman2, x, y, 160, 360);
+              context.drawImage(woman1, x, y);
+            } else if (remain < 20 ) {
+              context.drawImage(woman2, x, y);
+            } else if (remain < 30 ) {
+              context.drawImage(woman3, x, y);
+            } else if (remain < 40 ) {
+              context.drawImage(woman4, x, y);
+            } else if (remain >= 40 ) {
+              context.drawImage(woman5, x, y);
             }
           } else {
-            context.drawImage(woman, x, y, 160, 360);
+            context.drawImage(woman, x, y);
           }
 
           
           // so as the man reaches the girl.. if not still loop walking cycles
-          if (xMan + 110 > x) {
-            if (opacity > 0) {
-              context.globalAlpha = opacity;
-              opacity -= 0.02;
-              xMan = x - 110;
-              requestAnimationFrame(onAnimationFrame);
+          if (xMan + 60 > x) {
+            if (chaseOpacity > 0) {
+              context.globalAlpha = chaseOpacity;
+              chaseOpacity -= 0.02;
+              xMan = x - 60;
+              requestAnimationFrame(chaseScenario);
               document.body.onkeydown = function(event) {
                 if (event.keyCode == 39) {
                   console.log("pushed right");
@@ -95,20 +132,38 @@ $(document).ready(function() {
                 }
               }
             } else {
-              context.globalAlpha = 1;
-              context.fillStyle = "red";
-              context.font = "30px Helvetica";
-              //context.fillText("wow now save yourself :)", 170, 220);
-              $(".button2").animate({opacity: '1'}, 1000);
-
-              //click and scroll down???
+              context.clearRect(0, 0, canvas.width, canvas.height);
+              $(".scenario").fadeOut(1000);
+              $(".chase-fail").fadeIn(1000);
+              $(".button2").fadeIn(1000, function() {
+                $(".button2").click(function() {
+                  $(".chase-fail").fadeOut(500);
+                  $(".button2").fadeOut(500, function() {
+                    $(".canvaswalk").fadeIn(0, function() {
+                      requestAnimationFrame(walkAnimation);
+                    });
+                  });
+                });
+              });
 
             }
-
-          } else if (xMan > canvas.width) {
+          } else if (x > canvas.width) {
             context.clearRect(0, 0, canvas.width, canvas.height);
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            $(".scenario").fadeOut(1000);
+            $(".chase-success").fadeIn(1000);
+            $(".button2").fadeIn(1000, function() {
+              $(".button2").click(function() {
+                $(".chase-success").fadeOut(500);
+                $(".button2").fadeOut(500, function() {
+                  $(".canvaswalk").fadeIn(0, function() {
+                    requestAnimationFrame(walkAnimation);
+                  });
+                });
+              });
+            }); 
           } else {
-            requestAnimationFrame(onAnimationFrame);
+            requestAnimationFrame(chaseScenario);
           }
         };
 
@@ -124,45 +179,46 @@ $(document).ready(function() {
             rightKeyIsDown = true;
           }
         };
+
+
+        // =========== A NEW CANVAS!! ============
+
+        function walkAnimation() {
+      
+          context.clearRect(0, 0, canvas.width, canvas.height);
+
+          if (walkOpacity < 1) {
+            context.globalAlpha = walkOpacity;
+            walkOpacity += 0.003;
+            v -= 0.008;
+            xBehind += v;
+          //  requestAnimationFrame(walkAnimation);
+          }
+
+          context.drawImage(manbehind, xBehind, 50);
+          context.drawImage(womanfront, 350, 50);
+
+          
+          requestAnimationFrame(walkAnimation);
+
+          if (count < 80) {
+            count ++;
+          } else if (count < 150) {
+            $(".heartpink").animate({opacity: '1'}, 500)
+            $(".heartpink img").animate({width: '110px'}, 300)
+            $(".heartpink img").animate({width: '100px'}, 500)
+            count ++;
+          } else if (count < 250){
+            $(".braceletbutton").animate({opacity: '1'}, 1000);
+            count ++;
+          } else {
+            $(".braceletbutton-text").animate({opacity: '1'}, 1000);
+          } 
+        };
+
+
       });
     });
   });
 
-
-  // ======= THE BRACELET APPEARS =======
-  $(window).scroll(function() {
-    if($(popup).scrollTop() > 200) {
-      $(".front-woman").animate({top: '0', opacity: '0.5'}, 2000);
-    };
-    if($(popup).scrollTop() > 500) {
-      $(".back-man").animate({top: '-10px', opacity: '0.5', left: '200'}, 4000);
-      $(".back-man img").animate({width: '450px'}, 4000);
-    };
-    if($(popup).scrollTop() > 600) {
-      $(".bracelet1").animate({top: '300', opacity: '1'}, 2000);
-    };
-
-    if($(popup).scrollTop() > 900) {
-
-      $(".realheart").animate({opacity: '1'}, 2000, function() {
-        $(".realheart img").animate({width: '120px'}, 300, function() {
-          $(".realheart img").animate({width: '100px'}, 300, function() {
-            $(".realheart img").animate({width: '120px'}, 300, function() {
-              $(".bling").animate({opacity: '1'}, 300, function() {
-                $(".bling").animate({opacity: '0'}, 300, function() {
-                  $(".bling").animate({opacity: '1'}, 300, function() {
-                    $(".bling").animate({opacity: '0'}, 300, function() {
-                      $(".bling").animate({opacity: '1'}, 300)
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-    //hhhhearrrrrtbeat done!
-    };
-  });
 });
-
--- S1P2/js/main.js
+++ S1P2/js/main.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
     $("#braceletImg").attr("src", "img/" + $(this).val() + ".png");
   });
 
+  //preorder button changes
+
   $(".preorder").click(function() {
     $(this).css("background-image", "url(./img/submitted.png)");
   });
@@ -21,6 +23,8 @@ $(document).ready(function() {
         $(this).css("background-image", "url(./img/preorder.png)");
       };
     });
+
+  /* Scrolling Effects */
 
   //variable used to keep track of if the navbar has been clicked
   var navClicked = false;
@@ -82,15 +86,57 @@ $(document).ready(function() {
     });
     });
 
+  //arrow scroll up effect
+  $('.arrowUp').click(function(){
+    console.log("wow");
+        var nextSection = $(this).closest('section').next('section');
+        navClicked = true;
+
+        $('html, body').animate({
+            scrollTop: $(nextSection).offset().top
+        }, 1000);
+
+        //enable the scroll function listener when all animations are done
+      $(":animated").promise().done(function() {
+        setTimeout(function() {navClicked = false;}, 500);
+    });
+    });
+
 
   /* feature icons animation, for all icons */
   // mouse in to fade text in
   $('#features a').mouseover(function(){
-    $(this).siblings('p').fadeIn('slow')
+    $(this).siblings('h5, p, hr').fadeIn('slow')
   });
 
   // mouse out to fade text out
   $('#features a').mouseout(function(){
+    $(this).siblings('h5, p, hr').fadeOut('fast');
+  });
+
+  /* how it works animation, for all */
+    $('#howitworks a').mouseover(function(){
+    $(this).siblings('p').fadeIn('fast')
+  });
+
+  // mouse out to fade text out
+  $('#howitworks a').mouseout(function(){
     $(this).siblings('p').fadeOut('fast');
   });
+
+  // listens to when user change dropdown menu
+  $("#listingtype").change(function() {
+    // change the src of image to the user selected value
+    $("#pre-order h2").html($(this).val());
+  });
+
+  //social media button changes
+
+  $(".twitter").hover(
+    function() {
+      if ($(this).css("background-image").indexOf("twitter.png") == -1) {
+        $(this).css("background-image", "url(./img/twitter_hover.png)");
+      };
+    },
+  )
 });
